a175ac378fcb5eb38d053ebc7b1688caaa95965f
kwazny_252716 <kwazny_252716@e-science.pl>
2022-03-14 18:43:25 +0100

    created stub and signature for MainController

--- src/main/java/com/pwr/dpp/backlog/dpp/business/MainController.java
+++ src/main/java/com/pwr/dpp/backlog/dpp/business/MainController.java
@@ -1,7 +1,32 @@
 package com.pwr.dpp.backlog.dpp.business;
 
+import com.pwr.dpp.backlog.dpp.business.models.BoardModel;
+import com.pwr.dpp.backlog.dpp.business.models.CreateTaskModel;
+import com.pwr.dpp.backlog.dpp.business.models.LogInModel;
+import com.pwr.dpp.backlog.dpp.business.models.TaskDetailsModel;
+
 public class MainController {
-    public Object authorizationController(){
-        return null;
+    public BoardModel boardModel(){
+        throw new UnsupportedOperationException();
+    }
+
+    public LogInModel logInModel(){
+        throw new UnsupportedOperationException();
+    }
+
+    public CreateTaskModel createTaskModel(){
+        throw new UnsupportedOperationException();
+    }
+
+    public LoggedUserRepository loggedUserRepository(){
+        throw new UnsupportedOperationException();
+    }
+
+    public TaskDetailsModel taskDetailsModel(){
+        throw new UnsupportedOperationException();
+    }
+
+    public DatabaseHandler databaseHandler(){
+        throw new UnsupportedOperationException();
     }
 }

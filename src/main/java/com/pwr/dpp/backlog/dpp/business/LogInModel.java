4b3abd096a662668518893afe994c008fafac083
kwazny_252716 <kwazny_252716@e-science.pl>
2022-03-14 13:06:28 +0100

    added LoggedUserRepository split into interface and implementation, so that implementation can be different

--- src/main/java/com/pwr/dpp/backlog/dpp/business/LogInModel.java
+++ src/main/java/com/pwr/dpp/backlog/dpp/business/LogInModel.java
@@ -2,12 +2,34 @@ package com.pwr.dpp.backlog.dpp.business;
 
 public class LogInModel {
     private DatabaseHandler databaseHandler;
+    private LoggedUserRepository loggedUserRepository;
+
     public boolean logAs(String user) throws NoSuchUserException {
-        return databaseHandler.logAs(user);
+        boolean logInSuccessful =  databaseHandler.logAs(user);
+        if(logInSuccessful){
+            loggedUserRepository.setLoggedUser(user);
+        }
+        return logInSuccessful;
     }
 
     public void createUser(String username){
         if(!databaseHandler.createUser(username))
             throw new RuntimeException("Could not create user!");
+    }
+
+    public DatabaseHandler getDatabaseHandler() {
+        return databaseHandler;
+    }
+
+    public void setDatabaseHandler(DatabaseHandler databaseHandler) {
+        this.databaseHandler = databaseHandler;
+    }
+
+    public LoggedUserRepository getLoggedUserRepository() {
+        return loggedUserRepository;
+    }
+
+    public void setLoggedUserRepository(LoggedUserRepository loggedUserRepository) {
+        this.loggedUserRepository = loggedUserRepository;
     }
 }
--- src/main/java/com/pwr/dpp/backlog/dpp/business/LoggedUserRepository.java
+++ src/main/java/com/pwr/dpp/backlog/dpp/business/LoggedUserRepository.java
@@ -1,19 +1,7 @@
 package com.pwr.dpp.backlog.dpp.business;
 
-public class LoggedUserRepository {
-    private static final LoggedUserRepository instance = new LoggedUserRepository();
-    private String loggedUser;
+public interface LoggedUserRepository {
+    String getLoggedUser();
 
-    public static LoggedUserRepository getInstance(){
-        return instance;
-    }
-
-    public String getLoggedUser(){
-        return loggedUser;
-    }
-
-    public boolean logAs(String user){
-        loggedUser = user;
-        return true;
-    }
+    boolean setLoggedUser(String user);
 }

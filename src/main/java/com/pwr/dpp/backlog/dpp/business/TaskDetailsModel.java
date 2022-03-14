4642f90a85258855b8849ad929426a3900ea4757
kwazny_252716 <kwazny_252716@e-science.pl>
2022-03-14 16:42:28 +0100

    make TaskDetailsModel implement Observable

--- src/main/java/com/pwr/dpp/backlog/dpp/business/TaskDetailsModel.java
+++ src/main/java/com/pwr/dpp/backlog/dpp/business/TaskDetailsModel.java
@@ -1,13 +1,28 @@
 package com.pwr.dpp.backlog.dpp.business;
 
 import com.pwr.dpp.backlog.dpp.business.orm.Task;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 
-public class TaskDetailsModel {
+import java.util.LinkedList;
+import java.util.List;
+
+public class TaskDetailsModel implements Observable {
     private Task task;
     private DatabaseHandler databaseHandler;
+    private final List<InvalidationListener> listeners = new LinkedList<>();
+    private LoggedUserRepository loggedUserRepository;
 
     public void deleteTask(){
+        databaseHandler.deleteTask(task);
+        task = null;
+        invalidate();
+    }
 
+    public void addComment(String comment){
+        String user = loggedUserRepository.getLoggedUser();
+
+        throw new UnsupportedOperationException();
     }
 
     public DatabaseHandler getDatabaseHandler() {
@@ -16,5 +31,30 @@ public class TaskDetailsModel {
 
     public void setDatabaseHandler(DatabaseHandler databaseHandler) {
         this.databaseHandler = databaseHandler;
+    }
+
+    @Override
+    public void addListener(InvalidationListener invalidationListener) {
+        listeners.add(invalidationListener);
+    }
+
+    @Override
+    public void removeListener(InvalidationListener invalidationListener) {
+        listeners.remove(invalidationListener);
+    }
+
+    public void invalidate(){
+        for (InvalidationListener listener:
+                listeners) {
+            listener.invalidated(this);
+        }
+    }
+
+    public void setLoggedUserRepository(LoggedUserRepository loggedUserRepository) {
+        this.loggedUserRepository = loggedUserRepository;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
     }
 }

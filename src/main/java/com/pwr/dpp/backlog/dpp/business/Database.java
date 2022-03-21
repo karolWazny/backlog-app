4f4802046149c920ab0cad088dde02bff8b786a4
tgorniak_252697 <tgorniak_252697@e-science.pl>
2022-03-22 00:03:40 +0100

    Update Database.java implements interface SQLitehandler

--- src/main/java/com/pwr/dpp/backlog/dpp/business/Database.java
+++ src/main/java/com/pwr/dpp/backlog/dpp/business/Database.java
@@ -1,36 +1,39 @@
-import java.sql.*;
-import java.util.ArrayList;
+package com.pwr.dpp.backlog.dpp.business.orm;
 
-public class Database {
-    private static Connection con;
+import com.pwr.dpp.backlog.dpp.business.orm.SQLiteHandler;
+
+import java.sql.*;
+
+public class Database implements SQLiteHandler  {
+    private static Connection connection;
     //funkcje do komunikacji z bazą
     public static void Database() throws ClassNotFoundException, SQLException {
-        Connection con = null;
         Class.forName("org.sqlite.JDBC");
-        con = DriverManager.getConnection("jdbc:sqlite:C:/Users/Tomek/Desktop/test/test_db_connection/test.db"); //po jdbc:sqlite:C: wpisujemy scieżkę do bazy
+        connection = DriverManager.getConnection("jdbc:sqlite:" + System.getProperty("user.dir") + "db.db");
     }
 
     public static ResultSet getStatementValues(String sql) throws SQLException{
-        Statement statement = con.prepareStatement(sql);
+        Statement statement = connection.prepareStatement(sql);
         return statement.executeQuery(sql);
-
     }
 
     //funkcje wywolujące procedury
-    public static ResultSet getTasks() throws SQLException { //-zwraca zawartość tabel typu ResultSet
+    @Override
+    public ResultSet getTasks() throws SQLException { //-zwraca zawartość tabel typu ResultSet
         String procedure = "SELECT * FROM Tasks;";
         return getStatementValues(procedure);
     }
-
-    public static ResultSet getCommentary() throws SQLException{ //-zwraca zawartość tabel typu ResultSet
+    @Override
+    public ResultSet getComments() throws SQLException{ //-zwraca zawartość tabel typu ResultSet
         String sql = "SELECT * FROM Commentary;";
         return getStatementValues(sql);
     }
 
-    public static void createNewTask(String Title, String Name, String Category, String Description, String Date) throws SQLException {
+    @Override
+    public void addNewTask(String Title, String Name, String Category, String Description, String Date) throws SQLException {
         String sql = "INSERT INTO Tasks(Title, Name, Category, Description, Date)" +
-                     "VALUES(?, ?, ?, ?, ?);";
-        PreparedStatement statement = con.prepareStatement(sql);
+                "VALUES(?, ?, ?, ?, ?);";
+        PreparedStatement statement = connection.prepareStatement(sql);
         statement.setString(1, Title);
         statement.setString(2, Name);
         statement.setString(3, Category);
@@ -39,51 +42,53 @@ public class Database {
         statement.executeQuery();
     }
 
-    public static void deleteTask(String Task_id) throws SQLException {
+    @Override
+    public void deleteTask(int Task_id) throws SQLException {
         String sql = "DELETE FROM Commentary WHERE Task_id = ?" +
-                     "DELETE FROM Categories WHERE Task_id = ?" +
-                     "DELETE FROM Tasks WHERE Task_id = ?;";
-        PreparedStatement statement = con.prepareStatement(sql);
-        statement.setString(1, Task_id);
-        statement.setString(2, Task_id);
-        statement.setString(3, Task_id);
+                "DELETE FROM Categories WHERE Task_id = ?" +
+                "DELETE FROM Tasks WHERE Task_id = ?;";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, Integer.toString(Task_id));
+        statement.setString(2, Integer.toString(Task_id));
+        statement.setString(3, Integer.toString(Task_id));
         statement.executeQuery();
     }
 
-    public static void changeTaskCategory(String Task_id, String Category) throws SQLException {
+    @Override
+    public void changeTaskCategory(int Task_id, String Category) throws SQLException {
         String sql = "UPDATE Tasks SET Category = ? WHERE Task_id = ?" +
-                     "UPDATE Categories SET Category = ? WHERE Task_id = ?;";
-        PreparedStatement statement = con.prepareStatement(sql);
+                "UPDATE Categories SET Category = ? WHERE Task_id = ?;";
+        PreparedStatement statement = connection.prepareStatement(sql);
         statement.setString(1, Category);
-        statement.setString(2, Task_id);
+        statement.setString(2, Integer.toString(Task_id));
         statement.setString(3, Category);
-        statement.setString(4, Task_id);
+        statement.setString(4, Integer.toString(Task_id));
         statement.executeQuery();
     }
 
-    public static void changeUserTask(String Task_id, String Name) throws SQLException {
+    @Override
+    public void changeUserTask(int Task_id, String Name) throws SQLException {
         String sql = "UPDATE Tasks SET Name = ? WHERE Task_id = ?;";
-        PreparedStatement statement = con.prepareStatement(sql);
+        PreparedStatement statement = connection.prepareStatement(sql);
         statement.setString(1, Name);
-        statement.setString(2, Task_id);
+        statement.setString(2, Integer.toString(Task_id));
         statement.executeQuery();
     }
 
-    public static void deleteUserFromTask(String Task_id) throws SQLException {
+    @Override
+    public void deleteUserFromTask(int Task_id) throws SQLException {
         String sql = "UPDATE Tasks SET Name = ? WHERE Task_id = ?;";
-        PreparedStatement statement = con.prepareStatement(sql);
+        PreparedStatement statement = connection.prepareStatement(sql);
         statement.setString(1, null);
-        statement.setString(2, Task_id);
+        statement.setString(2, Integer.toString(Task_id));
         statement.executeQuery();
     }
 
-    public static void createNewUser(String Name) throws SQLException {
+    @Override
+    public void createNewUser(String Name) throws SQLException {
         String sql = "INSERT INTO Users(Name) VALUES(?);";
-        PreparedStatement statement = con.prepareStatement(sql);
+        PreparedStatement statement = connection.prepareStatement(sql);
         statement.setString(1, Name);
         statement.executeQuery();
-    }
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Database();
     }
 }

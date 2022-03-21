bde35ab010cbdf944b2c82ce02241af502445177
mgradys_241301 <241301@student.pwr.edu.pl>
2022-03-22 00:51:38 +0100

    Issue and merge templates, skeleton of BoardModel class test

---
+++ src/test/java/com/pwr/dpp/backlog/dpp/business/models/BoardModelTest.java
@@ -0,0 +1,16 @@
+package com.pwr.dpp.backlog.dpp.business.models;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+public class BoardModelTest {
+
+    @Mock
+    BoardModel mockBoard;
+
+    @Test
+    void listTasksWithCategoryTest() {
+
+    }
+}

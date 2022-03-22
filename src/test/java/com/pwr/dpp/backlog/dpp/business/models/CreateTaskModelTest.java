a3029eed52bd6cc412696cc52f449618ad8d093c
kwazny_252716 <63009342+karolWazny@users.noreply.github.com>
2022-03-22 02:57:10 +0100

    full tests for creating new task

--- src/test/java/com/pwr/dpp/backlog/dpp/business/models/CreateTaskModelTest.java
+++ src/test/java/com/pwr/dpp/backlog/dpp/business/models/CreateTaskModelTest.java
@@ -3,6 +3,7 @@ package com.pwr.dpp.backlog.dpp.business.models;
 import com.pwr.dpp.backlog.dpp.business.DatabaseHandler;
 import com.pwr.dpp.backlog.dpp.business.orm.Category;
 import com.pwr.dpp.backlog.dpp.business.orm.Task;
+import junit.framework.TestCase;
 import mockit.Mocked;
 import mockit.Verifications;
 import mockit.integration.junit4.JMockit;
@@ -11,7 +12,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 
 @RunWith(JMockit.class)
-public class CreateTaskModelTest {
+public class CreateTaskModelTest extends TestCase {
     @Test
     public void createTaskFullData(@Mocked DatabaseHandler mock){
 
@@ -33,5 +34,49 @@ public class CreateTaskModelTest {
             Assertions.assertEquals("username", task.getUser());
             Assertions.assertEquals("some title", task.getName());
         }};
+    }
+    @Test
+    public void createTaskMinimalData(@Mocked DatabaseHandler mock){
+
+        CreateTaskInfo info = new CreateTaskInfo();
+        info.setTitle("some title");
+
+        CreateTaskModel tested = new CreateTaskModel();
+        tested.setDatabaseHandler(mock);
+        tested.createTask(info);
+
+        new Verifications(){{
+            Task task;
+            mock.saveTask(task = withCapture());
+            Assertions.assertEquals("", task.getDescription());
+            Assertions.assertEquals(Category.OPEN, task.getCategory());
+            Assertions.assertTrue(task.getUser().equalsIgnoreCase("unassigned"));
+            Assertions.assertEquals("some title", task.getName());
+        }};
+    }
+    @Test
+    public void createTaskNullName(@Mocked DatabaseHandler mock){
+        try{
+            CreateTaskInfo info = new CreateTaskInfo();
+
+            CreateTaskModel tested = new CreateTaskModel();
+            tested.setDatabaseHandler(mock);
+            tested.createTask(info);
+            fail();
+        } catch(NullPointerException ignored){
+        }
+    }
+    @Test
+    public void createTaskBlankName(@Mocked DatabaseHandler mock){
+        try{
+            CreateTaskInfo info = new CreateTaskInfo();
+            info.setTitle("  \n \t   ");
+
+            CreateTaskModel tested = new CreateTaskModel();
+            tested.setDatabaseHandler(mock);
+            tested.createTask(info);
+            fail();
+        } catch(RuntimeException ignored){
+        }
     }
 }

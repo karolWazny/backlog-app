58dd6d8931c5ce4a2c2540cb14374adf1eb12648
karolWazny <63009342+karolWazny@users.noreply.github.com>
2022-03-22 01:10:02 +0100

    added maven dependencies for JMockit and JUnit

--- src/test/java/com/pwr/dpp/backlog/dpp/business/models/LogInModelTest.java
+++ src/test/java/com/pwr/dpp/backlog/dpp/business/models/LogInModelTest.java
@@ -1,4 +1,9 @@
 package com.pwr.dpp.backlog.dpp.business.models;
 
+import mockit.integration.junit4.JMockit;
+import org.junit.runner.RunWith;
+
+@RunWith(JMockit.class)
 public class LogInModelTest {
+
 }

15f88e9598b79c1c07adec97dd677bcda6828793
kwazny_252716 <63009342+karolWazny@users.noreply.github.com>
2022-03-22 02:10:28 +0100

    created test class

---
+++ src/test/java/com/pwr/dpp/backlog/dpp/business/models/CreateTaskModelTest.java
@@ -0,0 +1,8 @@
+package com.pwr.dpp.backlog.dpp.business.models;
+
+import mockit.integration.junit4.JMockit;
+import org.junit.runner.RunWith;
+
+@RunWith(JMockit.class)
+public class CreateTaskModelTest {
+}
